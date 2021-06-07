b1fc2e95090179dc93556cc756c26c3170d024ff
peeyushjangid <peeyushjangid17@gmail.com>
2021-06-07 13:19:34 +0530

    Bmi calculator

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#828282");
+final Color accentHexColor = HexColor("#0000FF");
